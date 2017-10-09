86d068c6e336eb83143b45857097d2f6f74a9aa7
Christian Haynes <06chaynes@gmail.com>
2017-10-09 11:45:07 -0400

    Added go file

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	r.Write(w)
+	fmt.Fprintf(w, "RemoteAddr: %s", r.RemoteAddr)
+}
+
+func main() {
+	port := flag.Int("port", 9090, "Listen on port")
+	flag.Parse()
+
+	http.HandleFunc("/", handler)
+	log.Println("Starting http server on port:", *port)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
+}
